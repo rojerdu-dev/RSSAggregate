e86e334067fcee4678fa8aa0b6ec871831123107
Rojer Du <rojerdu.dev@gmail.com>
2023-11-20 13:04:15 -0800

    Add GET all feeds functionality to handler and endpoint to router

--- handler_feed.go
+++ handler_feed.go
@@ -38,3 +38,13 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 
 	respondWithJSON(w, http.StatusCreated, databaseFeedToFeed(feed))
 }
+
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Could not get feeds: %v", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, databaseFeedsToFeeds(feeds))
+}
